7c30b489200a196013a1636f22b9f31d7cfb77e1
ZaycevaMarina <mari_minflash@yahoo.com>
2021-01-14 12:04:10 +0300

    Форма игры объявлена статическим членом класса. Добавлены события нажатия кнопок Начало игры, Рекорды(пока без выполнения действий), Выход. Файлы изображений добавлены в ресурсы проекта.

--- AsteroidGame/Program.cs
+++ AsteroidGame/Program.cs
@@ -6,6 +6,7 @@ namespace AsteroidGame
 {
     static class Program
     {
+        static Form __GameForm;
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -15,38 +16,61 @@ namespace AsteroidGame
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Form game_form = new Form();
-            game_form.Width = 800;
-            game_form.Height = 600;
-            game_form.Text = "Астероиды";
+            __GameForm = new Form();
+            __GameForm.Width = 800;
+            __GameForm.Height = 600;
+            __GameForm.Text = "Астероиды";
             //Добавление кнопок
             Button btnStartGame = new Button();
             btnStartGame.Text = "Начало игры";
             btnStartGame.Width = btnStartGame.Text.Length * 10;
-            game_form.Controls.Add(btnStartGame);
+            btnStartGame.Click += btnStartGameEvent;
+            btnStartGame.MouseClick += btnStartGameEvent;
+            __GameForm.Controls.Add(btnStartGame);
 
             Button btnRecords = new Button();
             btnRecords.Text = "Рекорды";
             btnRecords.Location = new Point(btnStartGame.Right + 1, 0);
             btnRecords.Width = btnStartGame.Width;
-            game_form.Controls.Add(btnRecords);
+            btnRecords.Click += btnRecordsEvent;
+            btnRecords.MouseClick += btnRecordsEvent;
+            __GameForm.Controls.Add(btnRecords);
 
             Button btnStopGame = new Button();
             btnStopGame.Text = "Выход";
             btnStopGame.Location = new Point(btnRecords.Right + 1, 0);
             btnStopGame.Width = btnStartGame.Width;
-            game_form.Controls.Add(btnStopGame);
+            btnStopGame.Click += btnStopGameEvent;
+            btnStopGame.MouseClick += btnStopGameEvent;
+            __GameForm.Controls.Add(btnStopGame);
             //Добавление на заставку имени автора
             Label lbl_author = new Label();
             lbl_author.Text = "(c) Зайцева Марина";
             lbl_author.Width = lbl_author.Text.Length * 7;
             lbl_author.Location = new Point(btnStopGame.Right + 10, 0);
-            game_form.Controls.Add(lbl_author);
+            __GameForm.Controls.Add(lbl_author);
 
-            SplashScreen.Initialize(game_form);
-            game_form.Show();
+            Application.Run(__GameForm);            
+        }
+
+        static void StartGame()
+        {
+            SplashScreen.Initialize(__GameForm);
+            __GameForm.Show();
             SplashScreen.Draw();
-            Application.Run(game_form);            
+        }
+        static void btnStartGameEvent(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        static void btnRecordsEvent(object sender, EventArgs e)
+        {//Добавить функционал после развития игры
+        }
+
+        static void btnStopGameEvent(object sender, EventArgs e)
+        {
+            __GameForm.Close();
         }
     }
 }
