eef503e62c42c81f9ae6f576d9c3454b9c13fb99
ZaycevaMarina <mari_minflash@yahoo.com>
2021-01-27 11:37:35 +0300

    Для обобщенной коллекции TList посчитано сколько раз каждый элемент встречается в данной коллекции, используя Linq.

---
+++ Lesson_4/Program.cs
@@ -0,0 +1,54 @@
+﻿/* Дана коллекция List<T>. 
+ * Требуется подсчитать, сколько раз каждый элемент встречается в данной коллекции:
+- для целых чисел;
+- для обобщенной коллекции;
+- *используя Linq.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //а)
+            Console.WriteLine("а) Подсчёт, количества встречаемости в List<T>");
+            List<int> List_int = new List<int>();
+            int len = 100;
+            Random rnd = new Random();
+            for(int i = 0; i < len; i++)
+                List_int.Add(rnd.Next(0, 10));
+            Dictionary<int, int> Frequency = new Dictionary<int, int>();
+            foreach (int count in List_int)
+            {
+                if (!Frequency.ContainsKey(count))
+                    Frequency[count] = 1;
+                else
+                    Frequency[count]++;
+            }
+            foreach(var fr in Frequency)
+            {
+                Console.WriteLine($"Число {fr.Key} встречается {fr.Value} раз");
+            }
+            //б)
+            Console.WriteLine("б) Для обобщенной коллекции на примере List<double> в классе TList<T>");
+            TList<double> T_List = new TList<double>();
+            T_List.T_List = new List<double>();
+            for (int i = 0; i < len; i++)
+                T_List.T_List.Add((double)rnd.Next(0,10)/10);
+            T_List.CalculateFrequency();
+            T_List.PrintFrequency();
+            
+            //в)
+            Console.WriteLine("в) используя Linq");
+            T_List.CalculateFrequencyLinq();
+            T_List.PrintFrequency();
+
+            Console.WriteLine("Для завершения нажмите любую кнопку...");
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ Lesson_4/TList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson_4
+{
+    class TList<T>
+    {
+        public List<T> T_List { get; set; }
+        int len = 100;
+        Dictionary<T, int> Frequency;
+
+        //public TList()
+        //    {
+        //        //for (int i = 0; i < len; i++)
+        //        //    T_List.Add(0);
+        //    }
+        public void CalculateFrequency()
+        {
+            if (T_List == null)
+                return;
+            Frequency = new Dictionary<T, int>();
+            foreach (T count in T_List)
+            {
+                if (!Frequency.ContainsKey(count))
+                    Frequency[count] = 1;
+                else
+                    Frequency[count]++;
+            }
+        }
+
+        public void PrintFrequency()
+        {
+            if (Frequency == null)
+                return;
+            foreach (var fr in Frequency)
+            {
+                Console.WriteLine($"Элемент {fr.Key} встречается {fr.Value} раз");
+            }
+        }
+
+        public void CalculateFrequencyLinq()
+        {
+            if (T_List == null)
+                return;
+            var group = from count in T_List
+                        group count by count;
+        }
+    }
+}
