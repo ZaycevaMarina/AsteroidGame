245af17fddd1b9021f207e3baf7e79d35839b51f
ZaycevaMarina <mari_minflash@yahoo.com>
2021-01-28 10:16:22 +0300

    Создана сущность Employee

---
+++ Lesson_5/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5
+{
+    public class Employee
+    {
+        static int id = 0;
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public double Salary { get; private set; }
+
+        public Employee(string name, int age, double salary)
+        {
+            Id = id++;
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+        public override string ToString()
+        {
+            return $"{Id}\t{Name}\t{Age}\t{Salary}";
+        }
+    }
+}
---
+++ Lesson_5/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace Lesson_5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Employee> items = new ObservableCollection<Employee>();
+        List<Employee> LEmployee = new List<Employee>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillList();
+        }
+        void FillList()
+        {
+            lbEmployee.ItemsSource = items;
+            LEmployee.Add(new Employee("Vasya", 22, 3000));
+            LEmployee.Add(new Employee("Petya", 25, 6000));
+            LEmployee.Add(new Employee("Kolya", 23, 8000));
+            foreach (Employee emp in LEmployee)
+                items.Add(emp);
+        }
+
+        private void lbEmployee_Selected(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(e.Source.ToString());
+        }
+
+        private void lbEmployee_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            MessageBox.Show(e.AddedItems[0].ToString());
+        }
+
+        private void btnAddEmployee(object sender, RoutedEventArgs e)
+        {
+            Employee emp = new Employee("Sergey", 26, 7000);
+            LEmployee.Add(emp);
+            items.Add(emp);
+        }
+    }    
+}
